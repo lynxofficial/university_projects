444fad4688fb2d5a255df65f0f09a06ba4917c45
Горшенин Егор Романович <200070@edu.psuti.ru>
2021-09-13 18:35:56 +0300

    Initial commit

---
+++ src/main/java/edu/psuti/pp/practice/Main.java
@@ -0,0 +1,10 @@
+package edu.psuti.pp.practice;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Hello!");
+        System.out.println("Shit");
+        System.out.println("Oh shit");
+    }
+}
---
+++ src/test/java/edu/psuti/pp/practice/MainTest.java
@@ -0,0 +1,13 @@
+package edu.psuti.pp.practice;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainTest {
+
+    @Test
+    public void main() {
+        assertFalse(false);
+    }
+}
