f789d36bf6ec11d1a7acbb1a0511d84ad52ea89a
Горшенин Егор Романович <200070@edu.psuti.ru>
2021-10-10 17:35:26 +0400

    Update DebitAccount.java

--- src/main/java/bank/DebitAccount.java
+++ src/main/java/bank/DebitAccount.java
@@ -3,20 +3,20 @@ package bank;
 
 public class DebitAccount extends Account {
 
-    public DebitAccount(long unique_number) {
-        super(unique_number);
+    public DebitAccount(long uniqueNumber) {
+        super(uniqueNumber);
     }
 
-    public DebitAccount(long unique_number, double balance) {
-        super(unique_number, balance);
+    public DebitAccount(long uniqueNumber, double balance) {
+        super(uniqueNumber, balance);
     }
 
-    public DebitAccount(long unique_number, double balance, double commission) {
-        super(unique_number, balance, commission);
+    public DebitAccount(long uniqueNumber, double balance, double commission) {
+        super(uniqueNumber, balance, commission);
     }
 
-    public DebitAccount(long unique_number, double balance, double commission, Currency currency) {
-        super(unique_number, balance, commission, currency);
+    public DebitAccount(long uniqueNumber, double balance, double commission, Currency currency) {
+        super(uniqueNumber, balance, commission, currency);
     }
 
     @Override
