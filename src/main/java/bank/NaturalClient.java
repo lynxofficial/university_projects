6736b5f0531e0ad5a1a1f230a36a6e00c6d9e6c4
Горшенин Егор Романович <200070@edu.psuti.ru>
2021-10-12 21:07:37 +0400

    Update NaturalClient.java

--- src/main/java/bank/NaturalClient.java
+++ src/main/java/bank/NaturalClient.java
@@ -2,14 +2,13 @@ package bank;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class NaturalClient implements Client {
     private String firstName;
     private String lastName;
     private String passport;
     private List<Account> accounts = new ArrayList<>();
-    private List<DebitAccount> debitAccounts = new ArrayList<>();
-    private List<CreditAccount> creditAccounts = new ArrayList<>();
 
     public NaturalClient(String firstName, String lastName, String passport) {
         this(firstName, lastName, passport, new ArrayList<>());
@@ -48,7 +47,7 @@ public class NaturalClient implements Client {
 
     public Account getAccount(long uniq) {
         for (Account account : accounts) {
-            if (account.getUnique_number() == uniq) {
+            if (account.getId() == uniq) {
                 return account;
             }
         }
@@ -59,7 +58,7 @@ public class NaturalClient implements Client {
         return accounts;
     }
 
-    public long amountBalance() {
+    public long totalBalance() {
         long sum = 0;
         for (Account account : accounts) {
             if (account.getBalance() > 0) {
@@ -69,7 +68,7 @@ public class NaturalClient implements Client {
         return sum;
     }
 
-    public List<Account> getPlusAccounts() {
+    public List<Account> getAccountsWithNonNegativeBalance() {
         List<Account> plusAccounts = new ArrayList<>();
         for (Account account : accounts) {
             if (account.getBalance() >= 0) {
@@ -81,7 +80,7 @@ public class NaturalClient implements Client {
 
     public void deleteAccount(long uniq) {
         for (Account account : accounts) {
-            if (account.getUnique_number() == uniq) {
+            if (account.getId() == uniq) {
                 accounts.remove(account);
                 break;
             }
@@ -94,57 +93,86 @@ public class NaturalClient implements Client {
 
 
     public void addDebitAccount(DebitAccount debitAccount) {
-        debitAccounts.add(debitAccount);
+        accounts.add(debitAccount);
     }
 
     public void addCreditAccount(CreditAccount creditAccount) {
-        creditAccounts.add(creditAccount);
+        accounts.add(creditAccount);
     }
 
-    public void reduceBalance(long uniq, double red) {
+    public void subtractBalance(long uniq, double red) {
         for (Account account : accounts) {
-            if (account.getUnique_number() == uniq) {
-                account.decreaseBalance(red);
+            if (account.getId() == uniq) {
+                account.subtractBalance(red);
                 break;
             }
         }
     }
 
-    public void increaseBalance(long uniq, double inc) {
+    public void refillBalance(long uniq, double inc) {
         for (Account account : accounts) {
-            if (account.getUnique_number() == uniq) {
-                account.replenishmentAccount(inc);
+            if (account.getId() == uniq) {
+                account.refillAccount(inc);
                 break;
             }
         }
     }
 
     public List<DebitAccount> getDebitAccounts() {
+        List<DebitAccount> debitAccounts = new ArrayList<>();
+        for (Account account : accounts) {
+            if (account instanceof DebitAccount) {
+                debitAccounts.add((DebitAccount) account);
+            }
+        }
         return debitAccounts;
     }
 
     public List<CreditAccount> getCreditAccounts() {
+        List<CreditAccount> creditAccounts = new ArrayList<>();
+        for (Account account : accounts) {
+            if (account instanceof CreditAccount) {
+                creditAccounts.add((CreditAccount) account);
+            }
+        }
         return creditAccounts;
     }
 
-    public double amountDebitAccounts() {
+    public double sumOfDebitAccounts() {
         double sum = 0;
-        for (DebitAccount debitAccount : debitAccounts) {
-            sum += debitAccount.getBalance();
+        for (Account account : accounts) {
+            if (account instanceof DebitAccount) {
+                sum += account.getBalance();
+            }
         }
         return sum;
     }
 
-    public double amountOfDebt() {
+    public double sumOfDebt() {
         double sum = 0;
-        for (CreditAccount creditAccount : creditAccounts) {
-            sum += creditAccount.getAssessed_commissions();
-            sum += creditAccount.getAssessed_percents();
-            if (creditAccount.getBalance() < 0) {
-                sum += creditAccount.getBalance();
+        for (Account account : accounts) {
+            if (account instanceof CreditAccount) {
+                sum += ((CreditAccount) account).getPlusCommissions();
+                sum += ((CreditAccount) account).getPlusPercents();
+                if (account.getBalance() < 0) {
+                    sum += account.getBalance();
+                }
             }
         }
         return sum;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        NaturalClient that = (NaturalClient) o;
+        return Objects.equals(firstName, that.firstName) && Objects.equals(lastName, that.lastName) && Objects.equals(passport, that.passport) && Objects.equals(accounts, that.accounts);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, passport, accounts);
     }
 
     @Override
