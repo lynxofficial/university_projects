8e83d77c3103f0afad19e896b9936e414ecaa992
Горшенин Егор Романович <200070@edu.psuti.ru>
2021-10-15 23:49:24 +0400

    Update CreditAccount.java

--- src/main/java/bank/CreditAccount.java
+++ src/main/java/bank/CreditAccount.java
@@ -5,13 +5,14 @@ import java.time.Month;
 import java.util.Objects;
 
 public class CreditAccount extends Account {
-    private static final double DEFAULT_PLUS_PERCENT = 0;
+    private static final double DEFAULT_ACCRUED_PERCENTS = 0;
     private static final double DEFAULT_PERCENT = 0;
     private static final long DEFAULT_CARD_LIMIT = 0;
+
     private double percent;
     private long cardLimit;
-    private double plusPercents;
-    private double plusCommissions;
+    private double accruedPercents;
+    private double accruedCommissions;
 
     public CreditAccount(long id) {
         this(id, DEFAULT_BALANCE, DEFAULT_COMMISSION, DEFAULT_CURRENCY, DEFAULT_PERCENT, DEFAULT_CARD_LIMIT);
@@ -51,32 +52,34 @@ public class CreditAccount extends Account {
         this.cardLimit = cardLimit;
     }
 
-    public double getPlusPercents() {
-        return plusPercents;
+    public double getAccruedPercents() {
+        return accruedPercents;
     }
 
-    public double getPlusCommissions() {
-        return plusCommissions;
+    public double getAccruedCommissions() {
+        return accruedCommissions;
     }
 
     public void increasePercent(int year) {
         LocalDate localDate = LocalDate.of(year, Month.DECEMBER.getValue(), Month.JANUARY.maxLength());
         if (getBalance() < cardLimit) {
-            plusPercents = (cardLimit - getBalance()) * (percent / localDate.getDayOfYear()) / 100;
+            accruedPercents = (cardLimit - getBalance()) * (percent / localDate.getDayOfYear()) / 100;
         }
     }
 
     @Override
     public void subtractCommissionFromBalance() {
-        plusCommissions += getCommission();
+        accruedCommissions += getCommission();
+        subtractBalance(getCommission());
+        setCommission(0);
     }
 
     @Override
     public void refillAccount(double sum) {
         super.refillAccount(sum);
-        subtractBalance(plusCommissions + plusPercents);
-        plusCommissions = DEFAULT_COMMISSION;
-        plusPercents = DEFAULT_PLUS_PERCENT;
+        subtractBalance(accruedCommissions + accruedPercents);
+        accruedCommissions = DEFAULT_COMMISSION;
+        accruedPercents = DEFAULT_ACCRUED_PERCENTS;
     }
 
     @Override
@@ -87,21 +90,21 @@ public class CreditAccount extends Account {
             return false;
         CreditAccount that = (CreditAccount) object;
         return Double.compare(that.percent, percent) == 0
-                && Double.compare(that.plusPercents, plusPercents) == 0
-                && Double.compare(that.plusCommissions, plusCommissions) == 0
+                && Double.compare(that.accruedPercents, accruedPercents) == 0
+                && Double.compare(that.accruedCommissions, accruedCommissions) == 0
                 && cardLimit == that.cardLimit;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(percent, plusPercents, plusCommissions, cardLimit);
+        return Objects.hash(percent, accruedPercents, accruedCommissions, cardLimit);
     }
 
     @Override
     public String toString() {
         return "percent: " + percent + "\n"
-                + "plusPercents: " + plusPercents + "\n"
-                + "plusCommissions: " + plusCommissions + "\n"
+                + "accruedPercents: " + accruedPercents + "\n"
+                + "accruedCommissions: " + accruedCommissions + "\n"
                 + "cardLimit: " + cardLimit + "\n";
     }
 }
