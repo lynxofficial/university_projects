e98809ea1293fb77557479e73629703c8631c17c
Горшенин Егор Романович <200070@edu.psuti.ru>
2021-10-15 23:48:12 +0400

    Update AccountNumberGenerator.java

--- src/main/java/bank/AccountNumberGenerator.java
+++ src/main/java/bank/AccountNumberGenerator.java
@@ -1,18 +1,20 @@
 package bank;
 
 public class AccountNumberGenerator {
-    private static long stat;
+    private final static long DEFAULT_ID = 0;
+
+    private static long id;
 
     public static long getNext() {
-        return stat++;
+        return id++;
     } // увеличение id счета
 
     public static long getCurrent() {
-        return stat;
+        return id;
     }
 
     public static void reset() {
-        stat = 0;
+        id = DEFAULT_ID;
     }
 
 }
