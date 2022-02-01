f83fcdb573a3d894f97863b7f6a54555ce605750
Горшенин Егор Романович <200070@edu.psuti.ru>
2022-02-01 16:22:43 +0300

    [RD21-34] Added AdmissionCommittee in new gradle-module

--- AdmissionCommittee/src/main/java/admissionCommittee/Test.java
+++ AdmissionCommittee/src/main/java/admissionCommittee/Test.java
@@ -1,10 +1,5 @@
 package admissionCommittee;
 
-import admissionCommittee.objects.Faculty;
-import admissionCommittee.postgreObjects.PostgreFaculty;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Test {
