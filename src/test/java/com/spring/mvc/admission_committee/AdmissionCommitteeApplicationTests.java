970d64f4b92d84452d97d37b62c6ffa18f8852b4
Egor Gorshenin <egor.gorshenin@mail.ru>
2023-01-15 22:04:22 +0400

    Initial commit

---
+++ src/test/java/com/spring/mvc/admission_committee/AdmissionCommitteeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.mvc.admission_committee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmissionCommitteeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
